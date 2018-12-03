fd974db988696de94025fce11b82574324e86f0b
Andreas Fleig <afleig@pivotal.io>
2018-12-03 16:48:42 +0100

    Run frontend tests through Gradle

--- ship.sh
+++ ship.sh
@@ -2,8 +2,8 @@
 set -e -o pipefail
 
 script_path=$(dirname $0)
-pushd $script_path/frontend
+pushd $script_path
     git pull -r
-    CI=1 npm test
+    CI=1 ./gradlew clean npm_test test
     git push
 popd
