6defad98cf58b66531f6522dd6856c28d2333f49
Andreas Fleig <afleig@pivotal.io>
2018-12-10 17:19:55 +0100

    deploy.sh: remove source maps

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ script_path=$(dirname $0)
 pushd $script_path/frontend
     npm run build
     cp ../Staticfile build
+    rm build/static/js/*.map
+    rm build/static/css/*.map
 popd
 
 pushd $script_path
