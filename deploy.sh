36f73d49bd698ecb3c1cf67c9af04b0861ac4100
Andreas Fleig <afleig@pivotal.io>
2018-12-12 10:09:23 +0100

    deploy.sh: do a blue/green deployment

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-script_path=$(dirname $0)
-pushd $script_path/frontend
-    npm run build
-    cp ../Staticfile build
-    rm build/static/js/*.map
-    rm build/static/css/*.map
-popd
+APP=epic-collection
 
-pushd $script_path
-    cf push
-popd
+main() {
+    script_path=$(dirname $0)
+    pushd $script_path/frontend
+        npm run build
+        cp ../Staticfile build
+        rm build/static/js/*.map
+        rm build/static/css/*.map
+    popd
+
+    pushd $script_path
+        rename_app_if_running
+        cf push
+        delete_old_instance_if_running
+    popd
+}
+
+rename_app_if_running() {
+    set +e
+    cf app "$APP" &>/dev/null
+    running="$?"
+    set -e
+
+    if [[ "$running" == "0" ]]
+    then
+        cf rename "$APP" "$APP-old"
+    fi
+}
+
+delete_old_instance_if_running() {
+    if [[ "$running" == "0" ]]
+    then
+        cf delete -f "$APP-old"
+    fi
+}
+
+main $*
